c75974b0825636cd9cf32a7dcfb8f38af73250e3
wiliamailysh <victor.douay+dev@gmail.com>
2019-06-12 10:14:40 +0200

    create todolist component file

---
+++ components/Todolist.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todolist_app">
+    <v-container>
+      <div>
+        Composant Todolist
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  }
+}
+</script>
