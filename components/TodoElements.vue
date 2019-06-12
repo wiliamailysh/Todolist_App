742a28f330db6b210a025062ae5457aa95335e37
wiliamailysh <victor.douay+dev@gmail.com>
2019-06-12 11:14:38 +0200

    create TodoElements component & add previous code from Todolist component

---
+++ components/TodoElements.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-card v-for="todo in allTodos" :key="todo.id">
+      {{ todo.text }}
+    </v-card>
+  </div>
+</template>
+
+<script>
+// import mapHelpers
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  components: {
+  },
+  computed: {
+    ...mapGetters({
+      allTodos: 'todolist/allTodos'
+    })
+  },
+  methods: {
+    ...mapActions({
+      addTodo: 'todolist/addTodo'
+    })
+  }
+}
+</script>
