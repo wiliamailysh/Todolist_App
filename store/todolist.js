fcf27d313e72662ef64ebc024ced93aefc005919
wiliamailysh <victor.douay+dev@gmail.com>
2019-06-12 11:00:28 +0200

    :linter: correct style

--- store/todolist.js
+++ store/todolist.js
@@ -9,20 +9,20 @@ export const getters = {
 }
 
 export const mutations = {
-  addTodo (state, payload) {
+  addTodo(state, payload) {
     state.list.unshift({
       text: payload.text,
-      id: state.list.length === 0 ? 1 : state.list[0].id + 1,
+      id: state.list.length === 0 ? 1 : state.list[0].id + 1
     })
   }
 }
 
 export const actions = {
-  addTodo (context, e) {
+  addTodo(context, e) {
     context.commit('addTodo', {
-      text: e.target.value,
+      text: e.target.value
     })
     // Clear input value after added task
-    e.target.value = ""
+    e.target.value = ''
   }
 }
