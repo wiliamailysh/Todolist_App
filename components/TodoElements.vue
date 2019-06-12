99964c902ad5c6cfbd856815c427008bfc92dc27
wiliamailysh <victor.douay+dev@gmail.com>
2019-06-12 11:24:40 +0200

    change layout using grid system & add checkbox to toggle done state

--- components/TodoElements.vue
+++ components/TodoElements.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <v-card v-for="todo in allTodos" :key="todo.id">
-      {{ todo.text }}
+      <v-layout row wrap>
+        <v-flex xs1>
+          <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
+        </v-flex>
+        <v-flex xs8>
+          {{ todo.text }}
+        </v-flex>
+        <v-flex xs3>
+
+        </v-flex>
+      </v-layout>
     </v-card>
   </div>
 </template>
@@ -20,7 +30,8 @@ export default {
   },
   methods: {
     ...mapActions({
-      addTodo: 'todolist/addTodo'
+      addTodo: 'todolist/addTodo',
+      toggle: 'todolist/toggle'
     })
   }
 }
